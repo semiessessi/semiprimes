7074d6acd17dfd060c8b23608ffe5e9ebb3de6dc
jheriko <semi@cranium-software.co.uk>
2020-10-11 00:35:13 +0100

    more mul testing

--- Code/Tests/Test_Multiplication.cpp
+++ Code/Tests/Test_Multiplication.cpp
@@ -2,11 +2,27 @@
 
 bool MulTests()
 {
+    const Number xBig1 =
+        0xFFFFFFFFFFFFFFFFz;
+    const Number xBig2 =
+        0xFFFFFFFFFFFFFFFF0z;
+    const uint64_t uSmall =
+        0x10;
+    if( ( xBig1 * uSmall ) != xBig2 )
+    {
+        return false;
+    }
+
     const Number xBig10Power =
         1000000000000000000000000000000000000000000000000000z;
     const Number xBigMultiplicand =
         111111111111111111111111111111111111111111111111111z;
     const Number xBigResult =
         111111111111111111111111111111111111111111111111111000000000000000000000000000000000000000000000000000z;
-    return ( xBig10Power * xBigMultiplicand ) == xBigResult;
+    if( ( xBig10Power * xBigMultiplicand ) != xBigResult )
+    {
+        return false;
+    }
+
+    return true;
 }
