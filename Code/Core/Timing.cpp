8fd389970f1c0cf417a7c87546ce7e57f26b3938
jheriko <semi@cranium-software.co.uk>
2020-10-31 00:03:03 +0000

    add more timing functionality for later...

--- Code/Core/Timing.cpp
+++ Code/Core/Timing.cpp
@@ -6,6 +6,7 @@
 
 static std::chrono::steady_clock::time_point sxStart;
 static std::chrono::steady_clock::time_point sxEnd;
+static std::chrono::steady_clock::time_point sxSampler;
 
 void StartTiming( const bool bVerbose )
 {
@@ -29,4 +30,13 @@ void StopTiming()
         "Took %lld.%09lld seconds\n",
         iDuration / 1000000000,
         iDuration % 1000000000 );
+}
+
+int64_t GetTimingNS()
+{
+    sxSampler = std::chrono::high_resolution_clock::now();
+
+    return
+        std::chrono::duration_cast< std::chrono::nanoseconds >(
+            sxSampler - sxStart ).count();
 }
--- Code/Core/Timing.h
+++ Code/Core/Timing.h
@@ -1,7 +1,10 @@
 #ifndef TIMING_H
 #define TIMING_H
 
+#include <cstdint>
+
 void StartTiming( const bool bVerbose );
 void StopTiming();
+int64_t GetTimingNS();
 
 #endif
